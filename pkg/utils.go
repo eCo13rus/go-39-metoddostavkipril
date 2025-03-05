16b374cd17f67179daec72d154c27c9202f58ebe
eCo13 <asd.mat@mail.ru>
2025-03-05 15:25:06 +0300

    fix

--- pkg/utils.go
+++ pkg/utils.go
@@ -5,6 +5,7 @@ import "strings"
 const vscSeparator = ","
 
 func someHelperFunction(s string) []string {
+
 	result := strings.Split(s, vscSeparator)
 	for i, part := range result {
 		result[i] = strings.ReplaceAll(part, ";", "")
